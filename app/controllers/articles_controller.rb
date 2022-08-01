7320c56e63cf6c2bc4d81b89dbe98f11b58528aa
Shahrukh <shahrukh.fazli@tekhqs.com>
2022-08-01 12:37:21 +0500

    refactoring code using DRY and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+
 	def index
 		@articles = Article.all
 	end
@@ -8,7 +11,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(params.require(:article).permit(:title, :description))
+		@article = Article.new(article_params)
 		if @article.save
 			flash[:notice] = "Article was created successfully."
 			# redirect_to article_path(@article)
@@ -19,16 +22,13 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-		@article = Article.find(params[:id])
 	end
 
 	def edit
-		@article = Article.find(params[:id])
 	end
 
 	def update
-		@article = Article.find(params[:id])
-		if @article.update(params.require(:article).permit(:title, :description))
+		if @article.update(article_params)
 			flash[:notice] = "Article was updated successfully."
 			# redirect_to article_path(@article)
 			redirect_to @article
@@ -38,9 +38,18 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
 		if @article.destroy
 			redirect_to articles_path
 		end
+	end
+
+	private
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :description)
 	end
 end
